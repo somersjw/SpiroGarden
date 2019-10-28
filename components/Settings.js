8c4f35e5e75f1e118e10625270a73ccedea03e4e
Matthew Chen <mattchen@umich.edu>
2019-10-28 12:58:20 -0400

    Alpha style changes

--- components/Settings.js
+++ components/Settings.js
@@ -17,7 +17,7 @@ export default class Settings extends React.Component {
                 <View style={styles.container}>
                     <Text style={styles.titlelarge}>Set Up Device</Text>
                     <Button title="Connect" onPress={this._onPressConnect} color="#229637"/>
-                    <Text style={styles.titlelarge}>Breathing Regiment</Text>
+                    <Text style={styles.titlelarge}>Breathing Regimen</Text>
                     <Text style={styles.heading2}>10 breaths per round</Text>
                     <Text style={styles.heading2}>3 rounds per day</Text>
                     <Text style={styles.heading2}>15 days</Text>
