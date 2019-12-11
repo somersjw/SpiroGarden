2f84ab86d591540bd9b656a6b3f980c6b57e0e59
Jack Somers <somersjw@umich.edu>
2019-12-10 19:27:28 -0500

    added shop layout

--- components/Shop.js
+++ components/Shop.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image, Animated, ScrollView } from "react-native";
 import MyHeader from './MyHeader';
 import styles from './styles';
 import { Button } from 'react-native-elements';
@@ -7,10 +7,10 @@ import { AsyncAlert, fetchSpiroData, getData, storeData, changePlant, initialize
 
 export default class Shop extends React.Component {
 
-
   constructor() {
     super();
     this.state = {money: 0}
+    this.buyItem
   }
   async componentDidMount() {
     let money = await getdatmoney(0);
@@ -21,17 +21,39 @@ export default class Shop extends React.Component {
 
     render (){
         return (
-        <View>
+        <View style={{flex: 1}}>
          <MyHeader activeIcon={true} navigation={this.props.navigation} title="Shop" />
             <View style={styles.container}>
               <Text style={[styles.titlemedium, styles.centered]}>Welcome to the Shop!</Text>
               <Text style={[styles.heading3, styles.centered]}>Your money is: ${this.state.money}</Text>
-               <Button
-                 title="Buy for $1000"
-                 onPress={this.buyitem}
-                 buttonStyle={styles.button}
-                 disabled={this.state.money < 1000}
-               />
+
+              <Animated.Image
+                style={{
+                  width: '30%',
+                  height: '30%',
+                }}
+                source={require('../assets/plant-medium.gif')}
+            />
+            <Button
+              title="Buy for $1000"
+              onPress={this.buyitem}
+              buttonStyle={styles.button}
+              disabled={this.state.money < 1000}
+            />
+
+            <Animated.Image
+                style={{
+                  width: '30%',
+                  height: '30%',
+                }}
+                source={require('../assets/plant2-medium.gif')}
+            />
+            <Button
+              title="Buy for $1000"
+              onPress={this.buyitem}
+              buttonStyle={styles.button}
+              disabled={this.state.money < 1000}
+            />
             </View>
         </View>
         );
